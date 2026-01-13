70fe1ffff4a0947f6bc60abb73351186ad9ccea6
Abdulaziz1214 <yormatov3@gmail.com>
2026-01-13 16:01:03 +0500

    nmadur change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      <h1>Bu header</h1>
+      <h1 className='w-full h-32 bg-amber-200 text-center'>Bu header</h1>
     </div>
   )
 }
