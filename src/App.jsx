983786e64f3ad258effa2f1fda8ec2cac1845648
Abdulaziz1214 <yormatov3@gmail.com>
2026-01-13 15:45:12 +0500

    behruz isquvar it

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-red-700'>Amir turshak</h1>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
